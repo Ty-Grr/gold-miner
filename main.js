9ff71a0234b83364d486ae0d32966217191c90b6
Ty-Grr <47638173+Ty-Grr@users.noreply.github.com>
2019-02-19 18:54:35 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-var gold = 0;
+// Variables
+var gold = 150;
 var gps = 0;
+var gpc = 1;
+var pickaxe = 0;
 var miner = 0;
 var coalmine = 0;
-var pickaxe = 0;
-var gpc = 1;
+var silvermine = 0;
 
+// Rounding Function
+function round(input){
+    var output = Math.round(input * 1000000)/1000000;
+	return output;
+}
+
+// Clicking function
 function goldClick(){
     gold += gpc;
-    document.getElementById("gold").innerHTML = gold;
+    document.getElementById("gold").innerHTML = round(gold);
 };
 
+// Auto Click function
 function gpsClick(){
   gold += gps;
-  document.getElementById("gold").innerHTML = gold;
+  document.getElementById("gold").innerHTML = round(gold);
   document.getElementById("gpc").innerHTML = gpc;
 }
 
-
-
+// Menu function
 function tab(tab) {
   document.getElementById("mineGoldMenu").style.display = "none"
   document.getElementById("shopMenu").style.display = "none"
@@ -25,58 +34,65 @@ function tab(tab) {
 }
 tab("mineGoldMenu")
 
-window.setInterval(function(){
-  gpsClick(gps)
-  document.getElementById("gps").innerHTML = gps;
-}, 1000);
 
+// Shop item functions
 function buyPickaxe(){
-  var pickaxecost = Math.floor(50 * Math.pow(2,pickaxe));
+  var pickaxecost = Math.floor(200 * Math.pow(3,pickaxe));
   if(gold >= pickaxecost){
     gpc *= 2;
     pickaxe = pickaxe + 1;
     gold = gold - pickaxecost
     document.getElementById("pickaxe").innerHTML = pickaxe;
-    document.getElementById("gold").innerHTML = gold;
+    document.getElementById("gold").innerHTML = round(gold);
   };
-  var nextPickaxeCost = Math.floor(50 * Math.pow(2, pickaxe));
+  var nextPickaxeCost = Math.floor(200 * Math.pow(3, pickaxe));
   document.getElementById("pickaxecost").innerHTML = nextPickaxeCost;
-
-}
+};
 
 function buyMiner(){
-    var minercost = Math.floor(10 * Math.pow(1.1,miner));
+    var minercost = Math.floor(10 * Math.pow(1.25,miner));
     if(gold >= minercost){
-        gps = gps + 1;
+        gps = gps + 0.1;
         miner = miner + 1;
     	  gold = gold - minercost;
         document.getElementById("miner").innerHTML = miner;
-        document.getElementById("gold").innerHTML = gold;
+        document.getElementById("gold").innerHTML = round(gold);
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,miner));
+    var nextCost = Math.floor(10 * Math.pow(1.25,miner));
     document.getElementById('minercost').innerHTML = nextCost;
 };
 
 function buyCoalMine(){
-    var coalminecost = Math.floor(150 * Math.pow(1.1,coalmine));
+    var coalminecost = Math.floor(150 * Math.pow(1.25,coalmine));
     if(gold >= coalminecost){
-        gps = gps + 10
+        gps = gps + 1;
         coalmine = coalmine + 1;
     	  gold = gold - coalminecost;
         document.getElementById('coalmine').innerHTML = coalmine;
-        document.getElementById('gold').innerHTML = gold;
+        document.getElementById("gold").innerHTML = round(gold);
     };
-    var nextCoalMineCost = Math.floor(150 * Math.pow(1.1,coalmine));
+    var nextCoalMineCost = Math.floor(150 * Math.pow(1.25,coalmine));
     document.getElementById('coalminecost').innerHTML = nextCoalMineCost;
 };
-var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem('goldMinerSave', JSON.stringify(gameData))
-}, 10000)
 
-var savegame = JSON.parse(localStorage.getItem("goldMinerSave"))
-if (savegame !== null) {
-  gameData = savegame
-}
+function buySilverMine(){
+    var silverminecost = Math.floor(500 * Math.pow(1.25,silvermine));
+    if(gold >= silverminecost){
+        gps = gps + 10;
+        silvermine = silvermine + 1;
+    	  gold = gold - silverminecost;
+        document.getElementById('silvermine').innerHTML = silvermine;
+        document.getElementById("gold").innerHTML = round(gold);
+    };
+    var nextSilverMineCost = Math.floor(500 * Math.pow(1.25,silvermine));
+    document.getElementById('silverminecost').innerHTML = nextSilverMineCost;
+};
 
 
-if (typeof savegame.dwarves !== "undefined") gameData.dwarves = savegame.dwarves;
+
+// Per Second Interval
+window.setInterval(function(){
+  gpsClick(gps)
+  document.getElementById("gps").innerHTML = round(gps);
+  document.getElementById('gold').innerHTML = round(gold);
+}, 1000);
