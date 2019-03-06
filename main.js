aa9096ad5bc687696ee6cb62e3a8d10139ebdaca
Ty-Grr <47638173+Ty-Grr@users.noreply.github.com>
2019-03-06 10:21:13 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 // Variables
-var gold = 0;
+var gold = 300000;
 var gps = 0;
 var gpc = 1;
+var multiplier = 1.07
 var pickaxe = 0;
 var miner = 0;
 var coalmine = 0;
 var silvermine = 0;
-var updateGUIInterval = setInterval("updateGUI()", 10);
+var ironmine = 0;
+var updateGUITimer = setInterval("updateGUI()", 10);
 
 // Rounding Function
 function round(input){
@@ -32,14 +34,17 @@ function updateGUI(){
   if(gold >= 10){
     document.getElementById("minerID").style.visibility = "visible"
   }
-  if(gold >= 150){
+  if(gold >= 100){
     document.getElementById("coalmineID").style.visibility = "visible"
   }
   if(gold >= 200){
     document.getElementById("pickaxeID").style.visibility = "visible"
   }
-  if(gold >= 1000){
+  if(gold >= 500){
     document.getElementById("silvermineID").style.visibility = "visible"
+  }
+  if(gold >= 2000){
+    document.getElementById("ironmineID").style.visibility = "visible"
   }
 }
 
@@ -47,7 +52,7 @@ function updateGUI(){
 
 // Shop item functions
 function buyPickaxe(){
-  var pickaxecost = Math.floor(200 * Math.pow(3,pickaxe));
+  var pickaxecost = Math.floor(200 * Math.pow(2,pickaxe));
   if(gold >= pickaxecost){
     gpc *= 2;
     pickaxe = pickaxe + 1;
@@ -55,12 +60,12 @@ function buyPickaxe(){
     document.getElementById("pickaxe").innerHTML = pickaxe;
     document.getElementById("gold").innerHTML = round(gold);
   };
-  var nextPickaxeCost = Math.floor(200 * Math.pow(3, pickaxe));
+  var nextPickaxeCost = Math.floor(200 * Math.pow(2, pickaxe));
   document.getElementById("pickaxecost").innerHTML = nextPickaxeCost;
 };
 
 function buyMiner(){
-    var minercost = Math.floor(10 * Math.pow(1.25,miner));
+    var minercost = Math.floor(10 * Math.pow(multiplier,miner));
     if(gold >= minercost){
         gps = gps + 0.1;
         miner = miner + 1;
@@ -68,12 +73,12 @@ function buyMiner(){
         document.getElementById("miner").innerHTML = miner;
         document.getElementById("gold").innerHTML = round(gold);
     };
-    var nextCost = Math.floor(10 * Math.pow(1.25,miner));
+    var nextCost = Math.floor(10 * Math.pow(multiplier,miner));
     document.getElementById('minercost').innerHTML = nextCost;
 };
 
 function buyCoalMine(){
-    var coalminecost = Math.floor(150 * Math.pow(1.25,coalmine));
+    var coalminecost = Math.floor(200 * Math.pow(multiplier,coalmine));
     if(gold >= coalminecost){
         gps = gps + 1;
         coalmine = coalmine + 1;
@@ -81,12 +86,12 @@ function buyCoalMine(){
         document.getElementById('coalmine').innerHTML = coalmine;
         document.getElementById("gold").innerHTML = round(gold);
     };
-    var nextCoalMineCost = Math.floor(150 * Math.pow(1.25,coalmine));
+    var nextCoalMineCost = Math.floor(200 * Math.pow(multiplier,coalmine));
     document.getElementById('coalminecost').innerHTML = nextCoalMineCost;
 };
 
 function buySilverMine(){
-    var silverminecost = Math.floor(1000 * Math.pow(1.25,silvermine));
+    var silverminecost = Math.floor(1000 * Math.pow(multiplier,silvermine));
     if(gold >= silverminecost){
         gps = gps + 10;
         silvermine = silvermine + 1;
@@ -94,8 +99,21 @@ function buySilverMine(){
         document.getElementById('silvermine').innerHTML = silvermine;
         document.getElementById("gold").innerHTML = round(gold);
     };
-    var nextSilverMineCost = Math.floor(1000 * Math.pow(1.25,silvermine));
+    var nextSilverMineCost = Math.floor(1000 * Math.pow(multiplier,silvermine));
     document.getElementById('silverminecost').innerHTML = nextSilverMineCost;
+};
+
+function buyIronMine(){
+    var ironminecost = Math.floor(5000 * Math.pow(multiplier,ironmine));
+    if(gold >= ironminecost){
+        gps = gps + 40;
+        ironmine = ironmine + 1;
+    	  gold = gold - ironminecost;
+        document.getElementById('ironmine').innerHTML = ironmine;
+        document.getElementById("gold").innerHTML = round(gold);
+    };
+    var nextIronMineCost = Math.floor(5000 * Math.pow(multiplier,ironmine));
+    document.getElementById('ironminecost').innerHTML = nextIronMineCost;
 };
 
 
