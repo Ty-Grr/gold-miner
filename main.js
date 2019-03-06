732045a67d808ff9add4e2b9a73d4ddce40c4f6c
Ty-Grr <47638173+Ty-Grr@users.noreply.github.com>
2019-03-06 12:29:36 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Variables
-var gold = 0;
-var gps = 0;
+var gold = 1000;
+var gpt = 0;
 var gpc = 1;
 var multiplier = 1.07
 var pickaxe = 0;
@@ -9,6 +9,7 @@ var coalmine = 0;
 var silvermine = 0;
 var ironmine = 0;
 var updateGUITimer = setInterval("updateGUI()", 10);
+var tickrate = 1000;
 
 // Rounding Function
 function round(input){
@@ -23,8 +24,8 @@ function goldClick(){
 };
 
 // Auto Click function
-function gpsClick(){
-  gold += gps;
+function gptClick(){
+  gold += gpt;
   document.getElementById("gold").innerHTML = round(gold);
   document.getElementById("gpc").innerHTML = gpc;
 }
@@ -34,16 +35,16 @@ function updateGUI(){
   if(gold >= 10){
     document.getElementById("minerID").style.visibility = "visible"
   }
-  if(gold >= 100){
+  if(gold >= 200){
     document.getElementById("coalmineID").style.visibility = "visible"
   }
   if(gold >= 200){
     document.getElementById("pickaxeID").style.visibility = "visible"
   }
-  if(gold >= 500){
+  if(gold >= 1000){
     document.getElementById("silvermineID").style.visibility = "visible"
   }
-  if(gold >= 2000){
+  if(gold >= 5000){
     document.getElementById("ironmineID").style.visibility = "visible"
   }
 }
@@ -67,7 +68,7 @@ function buyPickaxe(){
 function buyMiner(){
     var minercost = Math.floor(10 * Math.pow(multiplier,miner));
     if(gold >= minercost){
-        gps = gps + 0.1;
+        gpt = gpt + 0.1;
         miner = miner + 1;
     	  gold = gold - minercost;
         document.getElementById("miner").innerHTML = miner;
@@ -80,7 +81,7 @@ function buyMiner(){
 function buyCoalMine(){
     var coalminecost = Math.floor(200 * Math.pow(multiplier,coalmine));
     if(gold >= coalminecost){
-        gps = gps + 1;
+        gpt = gpt + 1;
         coalmine = coalmine + 1;
     	  gold = gold - coalminecost;
         document.getElementById('coalmine').innerHTML = coalmine;
@@ -93,7 +94,7 @@ function buyCoalMine(){
 function buySilverMine(){
     var silverminecost = Math.floor(1000 * Math.pow(multiplier,silvermine));
     if(gold >= silverminecost){
-        gps = gps + 10;
+        gpt = gpt + 10;
         silvermine = silvermine + 1;
     	  gold = gold - silverminecost;
         document.getElementById('silvermine').innerHTML = silvermine;
@@ -106,7 +107,7 @@ function buySilverMine(){
 function buyIronMine(){
     var ironminecost = Math.floor(5000 * Math.pow(multiplier,ironmine));
     if(gold >= ironminecost){
-        gps = gps + 40;
+        gpt = gpt + 40;
         ironmine = ironmine + 1;
     	  gold = gold - ironminecost;
         document.getElementById('ironmine').innerHTML = ironmine;
@@ -120,7 +121,8 @@ function buyIronMine(){
 
 // Per Second Interval
 window.setInterval(function(){
-  gpsClick(gps)
-  document.getElementById("gps").innerHTML = round(gps);
+  gptClick(gpt)
+  document.getElementById("gpt").innerHTML = round(gpt);
   document.getElementById('gold').innerHTML = round(gold);
-}, 1000);
+  document.getElementById("tickrate").innerHTML = tickrate;
+}, tickrate);
